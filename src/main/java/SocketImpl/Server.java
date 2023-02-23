2b2174f41be4384df7f2d08fd26b3975634d39fb
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-02-23 21:39:07 +0500

    Implementation of the method for sending commands to the server.

--- src/main/java/SocketImpl/Server.java
+++ src/main/java/SocketImpl/Server.java
@@ -19,13 +19,21 @@ public class Server {
     private void sendCommand(Command command, String args) throws IOException {
         if (Objects.isNull(command)) throw new RuntimeException("command null");
         if (Objects.isNull(args)) throw new RuntimeException("args null");
+        if (Objects.isNull(socket) | Objects.isNull(writer)) throw new RuntimeException("server not connected");
         final String endCommand = "\r\n";
         final String sep = " ";
+
         String commandLine = command.name() + sep + args + endCommand;
         writer.write(commandLine); // "command args \r\n"
         writer.flush();
+        if (DEBUG) System.out.printf("send command -> %s %n", commandLine);
+    }
+
+    private String readResponse() throws IOException {
+        String response = reader.readLine();
         if (DEBUG) {
-            System.out.printf("send command -> %s %n", commandLine);
+            System.out.printf("response -> %s %n", response);
         }
+        return response;
     }
 }
