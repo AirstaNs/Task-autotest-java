12b8f62ffe40aa4fdf3c192657de3028e149976f
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-02-23 21:24:31 +0500

    Implementation of the method for sending commands to the server.

--- src/main/java/SocketImpl/Server.java
+++ src/main/java/SocketImpl/Server.java
@@ -3,6 +3,7 @@ package SocketImpl;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Objects;
 
 
 public class Server {
@@ -14,4 +15,17 @@ public class Server {
 
     private static boolean DEBUG = true; //TODO REMOVE
 
+    /* method to send commands to ftp server */
+    private void sendCommand(Command command, String args) throws IOException {
+        if (Objects.isNull(command)) throw new RuntimeException("command null");
+        if (Objects.isNull(args)) throw new RuntimeException("args null");
+        final String endCommand = "\r\n";
+        final String sep = " ";
+        String commandLine = command.name() + sep + args + endCommand;
+        writer.write(commandLine); // "command args \r\n"
+        writer.flush();
+        if (DEBUG) {
+            System.out.printf("send command -> %s %n", commandLine);
+        }
+    }
 }
