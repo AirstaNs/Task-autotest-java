00e0f2fea2004b4716dc0997b2868e3edb384667
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-02-23 18:46:06 +0500

    Adding data reading from files for authorization.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Server server = getServer();
+        server.download("files/students.json");
+        server.upload("files/students.json");
+    }
+
+    private static Server getServer() {
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get("./hosts/beget.txt"))) {
+            String host = bufferedReader.readLine();
+            String log = bufferedReader.readLine();
+            String pass = bufferedReader.readLine();
+            return new Server(log, pass, host);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
