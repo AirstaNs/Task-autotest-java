4393ea693ee0c457b2b421a9a2d9716a685daa78
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-02-23 20:34:18 +0500

    create variables for working with a socket.

---
+++ src/main/java/SocketImpl/Server.java
@@ -0,0 +1,17 @@
+package SocketImpl;
+
+
+import java.io.*;
+import java.net.Socket;
+
+
+public class Server {
+    private Socket socket = null;
+
+    private BufferedReader reader = null;
+
+    private BufferedWriter writer = null;
+
+    private static boolean DEBUG = true; //TODO REMOVE
+
+}
