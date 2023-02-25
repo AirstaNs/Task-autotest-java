cbd2a5cef79bd4f77840edcecb8997f22bdbbd83
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-02-26 03:24:47 +0500

    fix quotes in the name of the student.

--- src/main/java/org/parser/JSON.java
+++ src/main/java/org/parser/JSON.java
@@ -3,8 +3,12 @@ package org.parser;
 
 import org.client.Student;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JSON {
 
@@ -27,7 +31,7 @@ public class JSON {
                 //Split the element into key-value pairs
                 String[] values = element.split(Utils.comma);
                 int id = Integer.parseInt(values[0].split(":")[1]);
-                String name = values[1].split(":")[1];
+                String name = values[1].split(":")[1].replaceAll("\"", "");
                 students1.add(new Student(id, name));
             }
         }
