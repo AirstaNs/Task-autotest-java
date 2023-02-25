42fc9dbccef6e295906f546bdd6a40a59d7bde8a
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-02-26 03:12:43 +0500

    Converting a method from json to a list of students.

---
+++ src/main/java/org/parser/JSON.java
@@ -0,0 +1,36 @@
+package org.parser;
+
+
+import org.client.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSON {
+
+    /*
+    Converting a method from json to a list of students.
+     */
+    public List<Student> fromJson(String json) {
+
+        //The structure to store the JSON
+        int startIndex = json.indexOf(Utils.openSquareBracket) + 1;
+        int endIndex = json.lastIndexOf(Utils.closeSquareBracket);
+        String studentListStr = json.substring(startIndex, endIndex).replaceAll("(\\s+|\\{)", "");
+        String[] jsonElements = studentListStr.split("\\}+\\,*"); // \{+.*\}+,*
+
+        List<Student> students1 = new ArrayList<>();
+        //Loop through each element
+        for (String element : jsonElements) {
+            //If the element is not empty
+            if (!element.isEmpty()) {
+                //Split the element into key-value pairs
+                String[] values = element.split(Utils.comma);
+                int id = Integer.parseInt(values[0].split(":")[1]);
+                String name = values[1].split(":")[1];
+                students1.add(new Student(id, name));
+            }
+        }
+        return students1;
+    }
+}
