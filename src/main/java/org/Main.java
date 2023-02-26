8e8828ff750c38ef576e7be882c36425af8c4fb6
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2023-02-26 23:39:07 +0500

    Adding the correct plugins to build the project.

---
+++ src/main/java/org/Main.java
@@ -0,0 +1,11 @@
+package org;
+
+
+import org.menu.FTPSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        FTPSystem ftpSystem = new FTPSystem();
+        ftpSystem.start();
+    }
+}
